f715c5be0e495ce04f73e7fe28bc2c31db0c2ff6
jennydev <jennydev2001@gmail.com>
2025-05-19 21:11:54 +0545

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import profile from "../assets/profile.webp"
 
 function About() {
   return (
-    <div className=' w-full h-screen'>
-            <p>Hero section</p>
+   <div className=' w-full sm:h-[200vh] md:h-[110vh] pt-20'>
+    <div className='flex justify-center pb-16'>
+      <h1 className='text-center text-2xl font-bold border-b-4 border-blue-400 border-spacing-2'>About Me</h1>
+
+    </div>
+        <div className='flex items-center  md:flex-row lg:flex-row sm:flex-col gap-10 '>
+
+          {/* picture */}
+          <div className=' md:w-[50%] lg:w-[50%] md:pl-32'>
+            <img src={profile}
+            className="w-[500px] h-[500px] rounded-3xl shadow-2xl shadow-blue-200   "
+            />
+          </div>
+
+          <div className='md:w-[50%] lg:w-[50%] flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold '>I'm a <span className='text-blue-500'>MERN Stack Developer</span> with a passion for building digital experiences...</h2>
+            <p>With over 1.5 years of experience in web development, I specialize in building responsive, user-centric applications across the full stack — from crafting smooth front-end interfaces to developing stable and scalable back-end systems.</p>
+            <p>I'm particularly drawn to working with emerging technologies like AI, machine learning, and deep learning. I enjoy solving real problems with clean, efficient code and constantly explore new tools and best practices to stay ahead in the field.</p>
+            
+            <div>
+              <p className='text-lg font-semibold'>Personal Information:</p>
+              <div className='flex flex-col gap-1 pt-2 pb-6'>
+              <p><span className='font-semibold'>Name:</span> Jenny Dev</p>
+              <p><span className='font-semibold'>Email:</span> jennydev2001@gmail.com</p>
+              <p><span className='font-semibold'>Location:</span> Kathmandu, Nepal</p>
+              <p><span className='font-semibold'>Phone:</span> +977-9844888976</p>
+              </div>
+              <button className='px-4 py-2 border bg-blue-500 text-white rounded-md'>My Resume</button>
+
+            </div>
+
+          </div>
+
+
+
+
+        </div>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,38 +1,43 @@
 import React from 'react'
 import { FiGithub } from "react-icons/fi";
 import { SlSocialLinkedin } from "react-icons/sl";
+import profile from "../assets/profile.webp"
 
 function Hero() {
   return (
-    <div className='bg-slate-50 dark:bg-gray-900 w-full h-screen '>
-        <div className='flex sm:flex-col pt-20'>
+    <div className='bg-slate-50 dark:bg-gray-900 w-full sm:h-[150vh] md:h-screen '>
+        <div className='flex items-center justify-around md:flex-row lg:flex-row sm:flex-col gap-20 pt-20'>
             {/* Introduction */}
-            <div className='flex flex-col gap-8'>
+            <div className='md:w-[45%] md:pt-10 flex flex-col gap-8'>
                 <p className='text-2xl font-serif text-blue-500'>Hello, I'm Jenny Dev</p>
                 <div>
-                <h1 className='text-5xl'>Full</h1>
-                <h1 className="indent-16 text-5xl">Stack</h1>
-                <h1 className="indent-36 text-5xl">Developer</h1>
+                <h1 className='text-5xl font-semibold'>MERN</h1>
+                <h1 className="indent-20 text-5xl font-semibold">Stack</h1>
+                <h1 className="indent-40 text-5xl font-semibold">Developer</h1>
                 </div>
-                <p className='text-gray-800 dark:text-white'>I build exceptional digital experiences with modern technologies. Specializing in creating responsive, user-friendly applications with clean and efficient code.</p>
+                <p className='text-xl pt-5 text-gray-800 dark:text-white'>I build exceptional digital experiences with modern technologies. Specializing in creating responsive, user-friendly applications with clean and efficient code.</p>
 
-                <div className='flex gap-6 px-4'>
+                <div className='flex gap-6 '>
                     <button className='bg-blue-500 text-white  px-4 py-2 rounded-md'>View Projects</button>
                     <button className='border border-blue-500 px-4 py-2 rounded-md'>Contact Me</button>
                 </div>
 
-                <div className='flex gap-6 px-5'>
-                    <FiGithub />
-                    <SlSocialLinkedin />
+                <div className='flex gap-6 '>
+                    <FiGithub className='text-2xl'/>
+                    <SlSocialLinkedin className='text-2xl'  />
 
                 </div>
             </div>
 
             {/* picture */}
-            <div>
-                
+            <div className='flex '>
+             <div className="rounded-full  border-[20px] border-blue-300">
+                <img src={profile}
+                className="w-80 h-80 rounded-full border-[20px] border-blue-400"
+                />
             </div>
 
+            </div>
         </div>
     </div>
   )
